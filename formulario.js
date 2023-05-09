4838682be487cb0acfb5ecb91b5c07ffbb6b0d87
karime <kaledon95@gmail.com>
2023-05-08 22:26:27 -0600

    Se arreglan errores en código en archivos html y js

--- formulario.js
+++ formulario.js
@@ -1,41 +1,64 @@
-var formulario = document.querySelector("#form")
 
-formulario.onsubmit = function(e) {
 
-  e.prevent();
-  
-  var n = formulario.elements[0]
-  var e = formulario.elements[1]
-  var na = formulario.elements[2]
+var formData = document.querySelector(".formData");
+var errorAlert = document.getElementById("errorAlert");
+var errorAlertText = document.getElementById("errorAlertText");
 
-  var nombre = n.value
-  var edad = e.value
+let listError = " ";
 
-  var i = na.selectedIndex
+formData.onsubmit = function (event) {
+  event.preventDefault();  
+  console.log("test");
+  var n = formData.elements[0];
+  var e = formData.elements[1];
+  var na = formData.elements[2];
+
+  var nombre = n.value;
+  var edad = e.value;
+
+  errorAlertText.innerHTML = "";    
+  errorAlert.style.display = "none";
+  let listError = "<ul>";
+
+  var i = na.selectedIndex;
   var nacionalidad = na.options[i].value
   console.log(nombre, edad)
   console.log(nacionalidad)
 
+
   if (nombre.length === 0) {
-    n.classList.add("error")
+    n.classList.add("error");
+    errorAlert.style.display = "block";
+    listError += "<li> Se debe escribir un nombre válido.</li>";   
+
   }
-  if (edad < 18 || edad > 120) {
+  if (edad < 18 || edad > 120) {  
     e.classList.add("error")
+    errorAlert.style.display = "block";
+    listError += "<li> Debes ser mayor de edad y/o escribir una edad válida.</li>";   
   }
 
 if (nombre.length > 0 
   && (edad > 18 
     && edad < 120) ) {
-  agregarInvitado(nombre, edad, nacionalidad)
+  agregarInvitado(nombre, edad, nacionalidad);
+  e.classList = "";
+  n.classList = "";
+  n.value = "";   
+  e.value = "";
   }
+
+  listError += "</ul>";
+  errorAlertText.insertAdjacentHTML("beforeend", listError);
+
 }
 
-var botonBorrar = document.createElement("button")
-botonBorrar.textContent = "Eliminar invitado"
-botonBorrar.id = "boton-borrar"
-var corteLinea = document.createElement("br")
-document.body.appendChild(corteLinea)
-document.body.appendChild(botonBorrar);
+// var botonBorrar = document.createElement("button")    //Repeat button Eliminar-Invitado
+// botonBorrar.textContent = "Eliminar invitado"
+// botonBorrar.id = "boton-borrar"
+// var corteLinea = document.createElement("br")
+// document.body.appendChild(corteLinea)
+// document.body.appendChild(botonBorrar);
 
 function agregarInvitado(nombre, edad, nacionalidad) {
 
@@ -55,20 +78,20 @@ function agregarInvitado(nombre, edad, nacionalidad) {
 var lista = document.getElementById("lista-de-invitados")
 
 var elementoLista = document.createElement("div")
-elementoLista.classList.added("elemento-lista")
+  elementoLista.classList.add("elemento-lista")   //Modified added to add
 lista.appendChild(elementoLista)
 
-var spanNombre = document.createElement("span")
-var inputNombre = document.createElement("input")
-var espacio = document.createElement("br")
-spanNombre.textContent = "Nombre: "
-inputNombre.value = nombre 
-elementoLista.appendChild(spanNombre)
-elementoLista.appendChild(inputNombre)
-elementoLista.appendChild(espacio)
+// var spanNombre = document.createElement("span")       // Repeat var of the name
+// var inputNombre = document.createElement("input")
+// var espacio = document.createElement("br")
+// spanNombre.textContent = "Nombre: "
+// inputNombre.value = nombre
+// elementoLista.appendChild(spanNombre)
+// elementoLista.appendChild(inputNombre)
+// elementoLista.appendChild(espacio)
 
 function crearElemento(descripcion, valor) {
-var spanNombre = document.createElement("span")
+  var spanNombre = document.createElement("span")      //This part show the name
 var inputNombre = document.createElement("input")
 var espacio = document.createElement("br")
 spanNombre.textContent = descripcion + ": "
@@ -91,7 +114,7 @@ elementoLista.appendChild(corteLinea)
 elementoLista.appendChild(botonBorrar);
 
  botonBorrar.onclick = function() {
-// this.parentNode.style.display = 'none';
+
 botonBorrar.parentNode.remove()
   }
 }
